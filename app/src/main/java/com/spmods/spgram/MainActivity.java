8898342f3c4237cb6a2e885809e7f57a68a952fc
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-05-16 17:18:12 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/spmods/spgram/MainActivity.java
@@ -0,0 +1,52 @@
+package com.spmods.spgram;
+
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import org.drinkless.td.libcore.telegram.Client;
+import org.drinkless.td.libcore.telegram.TdApi;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Client client;
+    private static final String TAG = "SPGram";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initTelegram();
+    }
+
+    private void initTelegram() {
+        client = Client.create(object -> {
+            if (object instanceof TdApi.UpdateAuthorizationState) {
+                handleAuth(((TdApi.UpdateAuthorizationState) object).authorizationState);
+            }
+        }, null, null);
+
+        TdApi.SetTdlibParameters params = new TdApi.SetTdlibParameters();
+        params.apiId = BuildVars.API_ID;
+        params.apiHash = BuildVars.API_HASH;
+        params.systemLanguageCode = "en";
+        params.deviceModel = "Android";
+        params.applicationVersion = BuildVars.VERSION;
+        params.databaseDirectory = getFilesDir().getAbsolutePath();
+
+        client.send(params, result -> Log.d(TAG, "TDLib ready"));
+    }
+
+    private void handleAuth(TdApi.AuthorizationState state) {
+        if (state instanceof TdApi.AuthorizationStateWaitPhoneNumber) {
+            Log.d(TAG, "Waiting for phone number");
+            // Phone number UI show කරන්න
+        } else if (state instanceof TdApi.AuthorizationStateWaitCode) {
+            Log.d(TAG, "Waiting for OTP");
+            // OTP UI show කරන්න
+        } else if (state instanceof TdApi.AuthorizationStateReady) {
+            Log.d(TAG, "Logged in!");
+            // Main chat screen show කරන්න
+        }
+    }
+}
