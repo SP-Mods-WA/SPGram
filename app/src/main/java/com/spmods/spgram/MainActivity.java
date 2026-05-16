cb0b5488e55591242d589cc435e34424863875b3
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-05-16 23:51:21 +0530

    Update MainActivity.java

--- app/src/main/java/com/spmods/spgram/MainActivity.java
+++ app/src/main/java/com/spmods/spgram/MainActivity.java
@@ -4,49 +4,55 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
-import org.drinkless.tdlib.Client;
-import org.drinkless.tdlib.TdApi;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Client client;
     private static final String TAG = "SPGram";
     private static String libraryError = null;
 
     static {
         try {
             System.loadLibrary("tdjni");
+            Log.d(TAG, "tdjni loaded OK");
         } catch (Throwable t) {
-            libraryError = t.toString() + "\n\n" + Log.getStackTraceString(t);
+            libraryError = "loadLibrary FAILED:\n" + t.toString();
+            Log.e(TAG, libraryError);
         }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CrashHandler.init(this);
         setContentView(R.layout.activity_main);
 
         if (libraryError != null) {
             new AlertDialog.Builder(this)
-                .setTitle("Library Load Error")
+                .setTitle("Library Error")
                 .setMessage(libraryError)
                 .setPositiveButton("OK", null)
                 .show();
             return;
         }
 
-        initTelegram();
+        try {
+            initTelegram();
+        } catch (Throwable t) {
+            new AlertDialog.Builder(this)
+                .setTitle("Init Error")
+                .setMessage(t.toString())
+                .setPositiveButton("OK", null)
+                .show();
+        }
     }
 
     private void initTelegram() {
-        client = Client.create(object -> {
-            if (object instanceof TdApi.UpdateAuthorizationState) {
-                handleAuth(((TdApi.UpdateAuthorizationState) object).authorizationState);
+        org.drinkless.tdlib.Client client = org.drinkless.tdlib.Client.create(object -> {
+            if (object instanceof org.drinkless.tdlib.TdApi.UpdateAuthorizationState) {
+                Log.d(TAG, "Auth state: " + object);
             }
         }, null, null);
 
-        TdApi.SetTdlibParameters params = new TdApi.SetTdlibParameters();
+        org.drinkless.tdlib.TdApi.SetTdlibParameters params = new org.drinkless.tdlib.TdApi.SetTdlibParameters();
         params.apiId = BuildVars.API_ID;
         params.apiHash = BuildVars.API_HASH;
         params.systemLanguageCode = "en";
@@ -54,16 +60,6 @@ public class MainActivity extends AppCompatActivity {
         params.applicationVersion = BuildVars.VERSION;
         params.databaseDirectory = getFilesDir().getAbsolutePath();
 
-        client.send(params, result -> Log.d(TAG, "TDLib ready"));
-    }
-
-    private void handleAuth(TdApi.AuthorizationState state) {
-        if (state instanceof TdApi.AuthorizationStateWaitPhoneNumber) {
-            Log.d(TAG, "Waiting for phone number");
-        } else if (state instanceof TdApi.AuthorizationStateWaitCode) {
-            Log.d(TAG, "Waiting for OTP");
-        } else if (state instanceof TdApi.AuthorizationStateReady) {
-            Log.d(TAG, "Logged in!");
-        }
+        client.send(params, result -> Log.d(TAG, "TDLib params set: " + result));
     }
 }
