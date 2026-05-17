f8acab4bc716d21bea607320599dcf9fa523e33d
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-05-17 15:02:54 +0530

    Delete app/src/main/java/com/spmods/spgram/BuildVars.java

--- app/src/main/java/com/spmods/spgram/BuildVars.java
+++
@@ -1,8 +0,0 @@
-package com.spmods.spgram;
-
-public class BuildVars {
-    public static final int API_ID = 35214748;
-    public static final String API_HASH = "2bc7633d816864cd22fe4173cedc67c9";
-    public static final String APP_NAME = "SPGram";
-    public static final String VERSION = "1.0";
-}
