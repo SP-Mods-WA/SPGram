29ebbfe336263f379f1108c29dd5294ba010c903
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-05-17 19:27:53 +0530

    Update Color.kt

--- app/src/main/java/com/spmods/spgram/ui/theme/Color.kt
+++ app/src/main/java/com/spmods/spgram/ui/theme/Color.kt
@@ -2,15 +2,28 @@ package com.spmods.spgram.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// SPGram Dark Theme Colors
-val Background    = Color(0xFF0A0A0A)
-val Surface       = Color(0xFF141414)
-val SurfaceVar    = Color(0xFF1E1E1E)
-val Primary       = Color(0xFF6B7FD4)   // soft blue-purple (avatar/accent)
-val OnPrimary     = Color(0xFFFFFFFF)
-val OnBackground  = Color(0xFFE8E8E8)
-val OnSurface     = Color(0xFFE8E8E8)
-val OnSurfaceVar  = Color(0xFF8A8A8A)
-val AvatarBg      = Color(0xFF2A2A3E)
-val ArchiveBg     = Color(0xFF1A1A2E)
-val Divider       = Color(0xFF1F1F1F)
+// Dark theme
+val DarkBackground   = Color(0xFF0A0A0A)
+val DarkSurface      = Color(0xFF141414)
+val DarkSurfaceVar   = Color(0xFF1E1E1E)
+val DarkOnBackground = Color(0xFFE8E8E8)
+val DarkOnSurface    = Color(0xFFE8E8E8)
+val DarkOnSurfaceVar = Color(0xFF8A8A8A)
+val DarkAvatarBg     = Color(0xFF2A2A3E)
+val DarkArchiveBg    = Color(0xFF1A1A2E)
+val DarkDivider      = Color(0xFF1F1F1F)
+
+// Light theme
+val LightBackground   = Color(0xFFFFFFFF)
+val LightSurface      = Color(0xFFF5F5F5)
+val LightSurfaceVar   = Color(0xFFEEEEEE)
+val LightOnBackground = Color(0xFF111111)
+val LightOnSurface    = Color(0xFF111111)
+val LightOnSurfaceVar = Color(0xFF777777)
+val LightAvatarBg     = Color(0xFFDDE0FF)
+val LightArchiveBg    = Color(0xFFE8EAF6)
+val LightDivider      = Color(0xFFE0E0E0)
+
+// Shared
+val Primary           = Color(0xFF6B7FD4)
+val OnPrimary         = Color(0xFFFFFFFF)
