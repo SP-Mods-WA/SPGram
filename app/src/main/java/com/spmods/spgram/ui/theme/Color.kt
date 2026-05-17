23add3fe53389969fd6f2ab4400760a111646887
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-05-17 18:35:05 +0530

    Update Color.kt

--- app/src/main/java/com/spmods/spgram/ui/theme/Color.kt
+++ app/src/main/java/com/spmods/spgram/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.spmods.spgram.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// SPGram Dark Theme Colors
+val Background    = Color(0xFF0A0A0A)
+val Surface       = Color(0xFF141414)
+val SurfaceVar    = Color(0xFF1E1E1E)
+val Primary       = Color(0xFF6B7FD4)   // soft blue-purple (avatar/accent)
+val OnPrimary     = Color(0xFFFFFFFF)
+val OnBackground  = Color(0xFFE8E8E8)
+val OnSurface     = Color(0xFFE8E8E8)
+val OnSurfaceVar  = Color(0xFF8A8A8A)
+val AvatarBg      = Color(0xFF2A2A3E)
+val ArchiveBg     = Color(0xFF1A1A2E)
+val Divider       = Color(0xFF1F1F1F)
