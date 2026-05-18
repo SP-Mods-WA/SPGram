8aad4be8b722f65398457088bff2ca089b4d8e73
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-05-18 15:42:12 +0530

    Add files via upload

---
+++ baselineprofile/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.test")
+    alias(libs.plugins.androidx.baselineprofile)
+}
+
+android {
+    namespace = "com.spmods.spgram.baselineprofile"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 25
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlin {
+        jvmToolchain(21)
+    }
+
+    targetProjectPath = ":app"
+}
+
+dependencies {
+    implementation(libs.androidx.benchmark.macro.junit4)
+    implementation(libs.androidx.uiautomator)
+    implementation(libs.androidx.test.ext.junit)
+}
