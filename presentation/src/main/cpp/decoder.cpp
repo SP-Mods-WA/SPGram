d06b8e1897b7e0e977066ec759e18211dd75dc14
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-05-21 12:54:03 +0530

    Update decoder.cpp

--- presentation/src/main/cpp/decoder.cpp
+++ presentation/src/main/cpp/decoder.cpp
@@ -6,7 +6,7 @@
 #include "vpx/vp8dx.h"
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_org_monogram_presentation_MainActivity_stringFromJNI(JNIEnv* env, jobject /* this */) {
+Java_com_spmods_spgram_presentation_MainActivity_stringFromJNI(JNIEnv* env, jobject /* this */) {
     const char* vpxVersion = vpx_codec_version_str();
 
     return env->NewStringUTF(vpxVersion);
