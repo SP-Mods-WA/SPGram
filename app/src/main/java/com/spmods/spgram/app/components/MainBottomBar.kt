ccfd58c1e17edddd40d66ba45e6475486dbcdb0e
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-06-02 01:35:43 +0530

    Update MainBottomBar.kt

--- app/src/main/java/com/spmods/spgram/app/components/MainBottomBar.kt
+++ app/src/main/java/com/spmods/spgram/app/components/MainBottomBar.kt
@@ -31,7 +31,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
