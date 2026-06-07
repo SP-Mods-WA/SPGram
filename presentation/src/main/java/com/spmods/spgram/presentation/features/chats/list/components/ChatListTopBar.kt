652cd4a658bb5fe17bb8542df1d5e81d37fadb33
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2026-06-07 10:02:53 +0530

    Update ChatListTopBar.kt

--- presentation/src/main/java/com/spmods/spgram/presentation/features/chats/list/components/ChatListTopBar.kt
+++ presentation/src/main/java/com/spmods/spgram/presentation/features/chats/list/components/ChatListTopBar.kt
@@ -1,5 +1,15 @@
 package com.spmods.spgram.presentation.features.chats.list.components
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
