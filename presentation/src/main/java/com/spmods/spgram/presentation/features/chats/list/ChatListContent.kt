cb94d199a4e0130454372ed71c3a1c7dde114dc9
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2026-06-08 13:44:20 +0530

    Add HorizontalDivider import to ChatListContent.kt

--- presentation/src/main/java/com/spmods/spgram/presentation/features/chats/list/ChatListContent.kt
+++ presentation/src/main/java/com/spmods/spgram/presentation/features/chats/list/ChatListContent.kt
@@ -14,6 +14,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.animation.slideInVertically
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.ExperimentalFoundationApi
