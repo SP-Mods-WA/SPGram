ba6a4648aa13899b6e83ae705ed097cd90961dfa
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2026-06-13 10:53:50 +0530

    Add selfDestructImmediately option to MessageSendOptions

--- domain/src/main/java/com/spmods/spgram/domain/models/MessageSendOptions.kt
+++ domain/src/main/java/com/spmods/spgram/domain/models/MessageSendOptions.kt
@@ -3,5 +3,6 @@ package com.spmods.spgram.domain.models
 data class MessageSendOptions(
     val silent: Boolean = false,
     val scheduleDate: Int? = null,
-    val sendAsDocument: Boolean = false
+    val sendAsDocument: Boolean = false,
+    val selfDestructImmediately: Boolean = false
 )
