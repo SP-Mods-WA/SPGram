a5f75a815e0fd13caf0846c8534c48f694ed9b73
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2026-07-02 19:42:09 +0530

    Add selfDestructSeconds to MessageSendOptions
    Added 'selfDestructSeconds' property to MessageSendOptions.

--- domain/src/main/java/com/spmods/spgram/domain/models/MessageSendOptions.kt
+++ domain/src/main/java/com/spmods/spgram/domain/models/MessageSendOptions.kt
@@ -4,5 +4,6 @@ data class MessageSendOptions(
     val silent: Boolean = false,
     val scheduleDate: Int? = null,
     val sendAsDocument: Boolean = false,
-    val selfDestructImmediately: Boolean = false
+    val selfDestructImmediately: Boolean = false,
+    val selfDestructSeconds: Int? = null  // null = Do Not Delete, 0 = View Once, 3/10/30 = timer
 )
