19072a54c2448381de3d1a3a6d28cc1677420968
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2026-07-25 21:01:24 +0530

    Add migration from 34 to 35 in dataModule.kt

--- data/src/main/java/com/spmods/spgram/data/di/dataModule.kt
+++ data/src/main/java/com/spmods/spgram/data/di/dataModule.kt
@@ -263,7 +263,8 @@ val dataModule = module {
                 SpgramMigrations.MIGRATION_30_31,
                 SpgramMigrations.MIGRATION_31_32,
                 SpgramMigrations.MIGRATION_32_33,
-                SpgramMigrations.MIGRATION_33_34
+                SpgramMigrations.MIGRATION_33_34,
+                SpgramMigrations.MIGRATION_34_35
             )
             .build()
     }
