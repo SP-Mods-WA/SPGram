e94316d3ddbae97f2c63cc2930dc1232cee10901
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2026-07-26 22:23:45 +0530

    Increment database version from 35 to 36

--- data/src/main/java/com/spmods/spgram/data/db/MonogramDatabase.kt
+++ data/src/main/java/com/spmods/spgram/data/db/MonogramDatabase.kt
@@ -60,7 +60,7 @@ import com.spmods.spgram.data.db.model.WallpaperEntity
         SponsorEntity::class,
         TextCompositionStyleEntity::class
     ],
-    version = 35,
+    version = 36,
     exportSchema = false
 )
 abstract class SpgramDatabase : RoomDatabase() {
